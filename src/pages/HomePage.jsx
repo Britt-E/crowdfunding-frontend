26c91b265ac5891c9d9e0f1055e2698bc10925b0
brittany evans <brittanyevans188@gmail.com>
2024-11-30 11:18:21 +1000

    async and fetch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
-import { allProjects } from "../data";
+import useProjects from "../hooks/use-projects";
+import ProjectCard from "../components/ProjectCard";
+import "./HomePage.css";
 
 function HomePage() {
+    const { projects, isLoading, error } = useProjects(); 
+    if (isLoading) {
+        return (<p>loading...</p>)
+    }
+    
+    if (error) {
+        return (<p>{error.message}</p>)
+    } 
     return (
-        <div>
-            {allProjects.map((projectData, key) => {
-                return <div key={key}>{projectData.title}</div>;
+        <div id="project-list">
+            {projects.map((projectData, key) => {
+                return <ProjectCard key={key} projectData={projectData} />;  
                 })}
         </div>
     );
