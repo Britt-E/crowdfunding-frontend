e773887797ba894eb66ef701f2082271c20db93a
brittany evans <brittanyevans188@gmail.com>
2024-11-30 12:57:58 +1000

    fetch and post

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import ProjectPage from "./pages/ProjectPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 import NavBar from "./components/NavBar.jsx";
+import { AuthProvider } from "./components/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,9 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProvider>
      {/* Here we wrap our app in the router provider so they render */}
      <RouterProvider router={router} />
+     </AuthProvider>
   </React.StrictMode>
 );
