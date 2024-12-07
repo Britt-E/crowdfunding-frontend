a47bd98c2a884829ad7d9e2977ceb971f3d1061a
brittany evans <brittanyevans188@gmail.com>
2024-12-07 10:01:08 +1000

    remove project input when not logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import useProjects from "../hooks/use-projects";
 import ProjectCard from "../components/ProjectCard";
 import CreateProjectForm from "../components/CreateProjectForm";
+import useAuth from "../hooks/use-auth.js";
+import { Link} from "react-router-dom";
 import "./HomePage.css";
 
 function HomePage() {
     const { projects, isLoading, error } = useProjects(); 
+    const {auth} = useAuth();
     if (isLoading) {
         return (<p>loading...</p>)
     }
@@ -19,7 +22,11 @@ function HomePage() {
                 return <ProjectCard key={key} projectData={projectData} />;  
                 })}
         </div>
-        <CreateProjectForm />
+        {auth.token ? (
+            <CreateProjectForm />
+                ) : (
+                <Link to="/login">Login to create your project</Link>
+                )}
         </div>
     );
     }
